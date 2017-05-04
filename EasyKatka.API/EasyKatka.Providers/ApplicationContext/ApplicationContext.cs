01c9cd0db4608f51094381d67264c5bedb9f6da9
Alexander <sashachechoitko@gmail.com>
2017-05-05 00:58:49 +0300

    Added UML and Explain Note

--- EasyKatka.API/EasyKatka.Providers/ApplicationContext/ApplicationContext.cs
+++ EasyKatka.API/EasyKatka.Providers/ApplicationContext/ApplicationContext.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EasyKatka.Models
+namespace EasyKatka.Providers.ApplicationContext
 {
-    class Lot
+    public class ApplicationContext
     {
     }
 }
--- EasyKatka.API/EasyKatka.Providers/Properties/AssemblyInfo.cs
+++ EasyKatka.API/EasyKatka.Providers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("EasyKatka.Models")]
+[assembly: AssemblyTitle("EasyKatka.Providers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EasyKatka.Models")]
+[assembly: AssemblyProduct("EasyKatka.Providers")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("ebee283e-4b43-46e6-8d45-117d09b4de2f")]
+[assembly: Guid("72ce416a-f3f6-44ac-9498-0d12cc6d629f")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
